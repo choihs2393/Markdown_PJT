b8b021a899e89b8912e720bb1a1c3a6fea783642
dkdlrnf0 <dkdlrnf0@gmail.com>
2020-08-05 16:28:39 +0900

    test

--- noteAPI/src/main/java/com/ggbg/note/config/RedisConfig.java
+++ noteAPI/src/main/java/com/ggbg/note/config/RedisConfig.java
@@ -1,8 +1,12 @@
 package com.ggbg.note.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -12,14 +16,27 @@ import com.ggbg.note.bean.Token;
 
 
 @Configuration
+@PropertySource("application.properties")
 public class RedisConfig {
+	@Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
 	
+    @Value("${spring.redis.password}")
+    private String redisPw;
 	// Redis의 Lettuce 사용
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(redisPw));
+		LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
 		return lettuceConnectionFactory;
 	}
+	
 	
 	
 	@Bean
