44d0b89b8ec6712fee4e069da513e5d3d865e984
ddb8036631 <ddb8036631@gmail.com>
2020-08-07 12:18:42 +0900

    Resolves S03P13B104-11 / drag & drop 구현

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -128,6 +128,71 @@ import LoginModal from "./components/LoginModal.vue"
 import SignupModal from "./components/SignupModal.vue"
 import LogoutModal from "./components/LogoutModal.vue"
 
+// 드래그 후 드랍을 하면,
+document.addEventListener('drop', (event) => {
+    var openedFileData = '';
+    var textingFileData = '';
+
+    event.preventDefault();
+    event.stopPropagation();
+    
+    remote.BrowserWindow.getFocusedWindow().webContents.executeJavaScript(`document.getElementById("editor_textarea").value`)
+    .then(result => {
+        textingFileData = result;
+
+        // console.log(typeof fileData);
+        // console.log(fileData);
+
+        // 작성중인 텍스트가 있다면, 저장할건지 먼저 물어본다.
+        if(textingFileData.length > 0) {
+          const options = {
+              type: "question",
+              title: "Question",
+              message: "Are you sure you want to quit without saving?",
+              detail: "Click the save button if you want to save this text to your md file",
+              buttons: ["Cancel", "Save"],
+              defaultId: 1
+          };
+    
+          // 동기식으로 처리해야 할듯?
+          if(remote.dialog.showMessageBoxSync(options) == 1) {
+            remote.dialog.showSaveDialog(
+              {
+                title: "파일 저장하기!!!",
+                filters: [
+                  { name: 'Markdown', extensions: ['md'] },
+                ],
+                message: "TEST"
+              }
+            )
+            .then(result => {
+              var fileName = result.filePath;
+              fs.writeFile(fileName, textingFileData, (err) => {
+  
+              })
+            });
+          }
+        } // 작성중인 텍스트가 있다는 조건문
+    });
+
+    // 저장한 후에 열기.
+    for (const f of event.dataTransfer.files) { 
+      // Using the path attribute to get absolute file path 
+      // console.log('File Path of dragged files: ', f.path);
+      
+      fs.readFile(f.path, 'utf8', (err, data) => {
+        if(err) throw err;
+        // console.log(data);
+        // fileData = data;
+        let openedFileData = data;
+        // console.log("openedFileData : " + openedFileData);
+
+        let win = remote.BrowserWindow.getFocusedWindow();
+        win.webContents.send("ping", openedFileData);
+      });
+    }
+});
+
 export default {
   name: "App",
   created() {
@@ -168,7 +233,7 @@ export default {
   },
   methods: {
     showOpenDialog() {
-      console.log("showOpenDialog() 호출됨.");
+      // console.log("showOpenDialog() 호출됨.");
 
       remote.dialog.showOpenDialog(remote.BrowserWindow.getFocusedWindow(),
         {
@@ -180,8 +245,8 @@ export default {
 
         var folderName = folderFullPath.substring(folderFullPath.lastIndexOf("\\") + 1);
 
-        console.log("folderFullPath : " + folderFullPath);
-        console.log("folderName : " + folderName);
+        // console.log("folderFullPath : " + folderFullPath);
+        // console.log("folderName : " + folderName);
 
         this.folders = [];
         this.folders.push({icon: 'folder',  iconClass: 'grey lighten-1 white--text', title: folderName});
@@ -189,10 +254,10 @@ export default {
         fs.readdir(folderFullPath, (err, fileList) => {
           this.files = [];
 
-          console.log(fileList);
+          // console.log(fileList);
 
           for(var i = 0; i < fileList.length; i++) {
-            console.log(folderFullPath + "\\" + fileList[i]);
+            // console.log(folderFullPath + "\\" + fileList[i]);
             if(fileList[i].substring(fileList[i].length-3, fileList[i].length) === '.md')
               this.files.push({ icon: 'assignment', iconClass: 'blue white--text', title: fileList[i], fileFullPath: folderFullPath + "\\" + fileList[i]});
 
