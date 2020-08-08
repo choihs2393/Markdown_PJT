79ebda2e2dcb69201d87d217930dcc0c5973ae70
Hona <choihs2393@gmail.com>
2020-08-08 21:25:00 +0900

    Move Style tag

--- front_end/src/components/SideBar.vue
+++ front_end/src/components/SideBar.vue
@@ -200,5 +200,37 @@ export default {
 </script>
 
 <style>
-
+  .v-list {
+    padding: 0;
+  }
+  .sidesubheader {
+      border-bottom: solid 3px rgb(100, 93, 102);
+      display: inline-block;
+      margin: 2.5em 0 0 0;
+      padding: 0 0.75em 0.5em 0;
+  }
+  .v-list-item {
+      margin: 0.5em 2em 0;
+      padding: 1em 0;
+      height: 1em;
+  }
+  .v-list-item:not(:last-child) {
+      font-family: 'Roboto Slab', sans-serif;
+      border-bottom: solid 1px rgba(210, 215, 217, 0.75);
+  }
+  .v-subheader:not(.sidesubheader) {
+      font-size: 1.25em;
+      color: rgb(95, 90, 97) !important;
+      border-bottom: solid 2px rgb(83, 81, 83);
+      display: inline-block;
+      margin: 0.5em;
+      padding: 0.75em 0;
+  }
+  /* .theme--light.v-list-item:not(.v-list-item--active) {
+      color: #615f75 !important;
+      font-size:0.5em;
+  }
+  .theme--light.v-list-item:not(.v-list-item--active):not(.v-list-item--disabled):hover {
+      color: rgb(214, 198, 219) !important;
+  } */
 </style>
