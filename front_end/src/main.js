c13e357a8adfd0ce0c9d8da9a983f2906165732a
Hona <choihs2393@gmail.com>
2020-08-12 21:46:02 +0900

    Fix: update_token

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -9,20 +9,20 @@ import SERVER from '@/api/spring'
 
 Vue.config.productionTip = false;
 
-Object.defineProperty(Date.prototype, 'YYYYMMDDHHMMSS', {
-  value: function() {
-    function pad2(n) {  // always returns a string
-      return (n < 10 ? '0' : '') + n;
-    }
+// Object.defineProperty(Date.prototype, 'YYYYMMDDHHMMSS', {
+//   value: function() {
+//     function pad2(n) {  // always returns a string
+//       return (n < 10 ? '0' : '') + n;
+//     }
     
-    return this.getFullYear() +
-    pad2(this.getMonth() + 1) + 
-    pad2(this.getDate()) +
-    pad2(this.getHours()) +
-    pad2(this.getMinutes()) +
-    pad2(this.getSeconds());
-  }
-});
+//     return this.getFullYear() +
+//     pad2(this.getMonth() + 1) + 
+//     pad2(this.getDate()) +
+//     pad2(this.getHours()) +
+//     pad2(this.getMinutes()) +
+//     pad2(this.getSeconds());
+//   }
+// });
 
 // Vue.prototype.$http = axios;
 // axios.defaults.timeout = 10000;
@@ -45,45 +45,50 @@ axios.interceptors.request.use(
     function(response) {
       // console.log('response interceptor success!!');
 
-      const nowTime = Number(new Date().YYYYMMDDHHMMSS())
-      const expiraionTime = Number(localStorage.getItem('access-token-expiraion-date'))
+      // const nowTime = Number(new Date().YYYYMMDDHHMMSS())
+      // const expiraionTime = Number(localStorage.getItem('access-token-expiraion-date'))
 
       // console.log('nowTime:', nowTime)
       // console.log('expiraionTime:', expiraionTime)
-      // console.log('expiraionTime - 5분:', expiraionTime - 500)
-      // console.log(nowTime > expiraionTime - 500 && nowTime < expiraionTime)
+      // console.log('남은시간', expiraionTime - nowTime)
 
-      
-      
+      // if (nowTime > expiraionTime - 500 && nowTime < expiraionTime) {
+      //   console.log('기간 만료되기 전 토큰 갱신!!')
+
+      // axios.post(SERVER.URL + SERVER.ROUTES.newATBA, null, { credentials: true })
+      //   .then(res => {
+      //     console.log('res:', res) 
+      //     store.commit('SET_TOKEN', res.headers)
+      //   })
+      //   .catch(err => console.error(err.response.data))
+      // }
       return response
     },
+
     function(err) {
-    // console.log('response interceptor error!!');
+      // console.log('response interceptor error!!');
 
-    const errorAPI = err.config
+      const errorAPI = err.config
 
-    if (err.response.status===401 && errorAPI.retry===undefined) {
-      // console.log('response interceptor 401 error!!');
+      if (err.response.status===401 && errorAPI.retry===undefined) {
+        // console.log('response interceptor 401 error!!');
 
-      errorAPI.retry = true
+        errorAPI.retry = true
 
-      if (!!localStorage.getItem('authorization')) {
-        return axios.post(SERVER.URL + SERVER.ROUTES.newATBR, null, { headers: { RefreshToken: store.state.refreshToken } })
-          .then(res => {
-            
-            console.log('토큰 갱신!!')
-            // console.log(res.headers)
-            store.commit('SET_TOKEN', res.headers)
-
-            // store.state.authorization = res.headers['authorization']
-            // localStorage.setItem('authorization', store.state.authorization)
-            // console.log('errorAPI', errorAPI)
-            return axios(errorAPI)
-          })
+        if (!!localStorage.getItem('authorization')) {
+          return axios.post(SERVER.URL + SERVER.ROUTES.newATBR, null, { headers: { RefreshToken: store.state.refreshToken } })
+            .then(res => {
+              
+              // console.log('기간 만료 후 토큰 갱신!!')
+              // console.log(res.headers)
+              store.commit('SET_TOKEN', res.headers)
+              // console.log('errorAPI', errorAPI)
+              return axios(errorAPI)
+            })
+        }
       }
-    }
-    return Promise.reject(err)
-  })
+      return Promise.reject(err)
+    })
 
 new Vue({
   // router,
