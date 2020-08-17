307bebb1b4f90cc01648cda143858bb2401ecf43
kimmuseong <ddb8036631@gmail.com>
2020-08-18 03:06:29 +0900

    백 코드 수정

--- noteAPI/src/main/java/com/ggbg/note/service/ISocketService.java
+++ noteAPI/src/main/java/com/ggbg/note/service/ISocketService.java
@@ -5,6 +5,6 @@ import java.util.Map;
 import com.ggbg.note.domain.dto.NoteDetailDTO;
 
 public interface ISocketService {
-	public Map<String, Object> occupy(int accountNo, int bandNo, int noteNo, String subject, String content, String occupiedName); // 혹시 갖고오는 부분에 boolean 처리를 해서 실패했는지 성공했는지에 대해서도 알려줘야하는지???
-	public Map<String, Object> vacate(int accountNo, int bandNo, int noteNo, String subject, String content);
+	public Map<String, String> occupy(int accountNo, int bandNo, int noteNo, String subject, String content, String occupiedName); // 혹시 갖고오는 부분에 boolean 처리를 해서 실패했는지 성공했는지에 대해서도 알려줘야하는지???
+	public Map<String, String> vacate(int accountNo, int bandNo, int noteNo, String subject, String content);
 }
