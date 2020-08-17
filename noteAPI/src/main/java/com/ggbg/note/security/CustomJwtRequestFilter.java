55405a57fe412581f95558a5584c9dd87b78715a
ddb8036631 <ddb8036631@gmail.com>
2020-08-17 13:29:21 +0900

    로그인이 안되는 이슈가 있어서 이 한줄 주석처리했습니다.

--- noteAPI/src/main/java/com/ggbg/note/security/CustomJwtRequestFilter.java
+++ noteAPI/src/main/java/com/ggbg/note/security/CustomJwtRequestFilter.java
@@ -69,7 +69,7 @@ public class CustomJwtRequestFilter extends OncePerRequestFilter {
 			}
 		} else {
 			logger.warn("JWT Token does not begin with Bearer String");
-			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 		}
 		
 		if (email == null) {
