f0b11c7ca02f54b0b578bef4006e5340944843da
Hona <choihs2393@gmail.com>
2020-08-17 23:53:22 +0900

    Local mode setting

--- front_end/src/components/InviteModal.vue
+++ front_end/src/components/InviteModal.vue
@@ -91,8 +91,8 @@ export default {
     inviteBandMember() {
       this.$store.dispatch("findAccountList", this.search);
 
-      // const serverURL = "http://localhost:8080/noteAPI/ws";
-      const serverURL = "http://i3b104.p.ssafy.io:80/noteAPI/ws";
+      const serverURL = "http://localhost:8080/noteAPI/ws";
+      // const serverURL = "http://i3b104.p.ssafy.io:80/noteAPI/ws";
       let socket = new SockJS(serverURL);
       this.stompClient = Stomp.over(socket);
 
